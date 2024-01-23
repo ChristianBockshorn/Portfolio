325480b7ab3db2416e25bb51b4bd9c21af8a9acb
Christian Bockshorn <bockshorn@gmx.net>
2024-01-23 15:21:05 +0100

    add landingPage

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    AboveTheFoldComponent,
+  ],
   templateUrl: './landingPage.component.html',
   styleUrl: './landingPage.component.scss'
 })
