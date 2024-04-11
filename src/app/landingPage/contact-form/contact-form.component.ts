ea55aafe7dbe215c5fd179f09fcd67b0aaa67697
Christian Bockshorn <bockshorn@gmx.net>
2024-04-11 09:36:51 +0200

    edit form

--- src/app/landingPage/contact-form/contact-form.component.ts
+++ src/app/landingPage/contact-form/contact-form.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  @ViewChild('sendButton') sendButton!: ElementRef;
 
+  async sendMail() {
+    console.log('Sending Mail ', this.myForm);
+
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
+
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+    //Animation anzeigen
+
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('email', emailField.value);
+    fd.append('message', messageField.value);
+
+    // Senden
+    await fetch('https://f0155033@christian-bockshorn.developerakademie.net/send_mail/send_mail.php', {
+      method: 'POST',
+      body: fd
+    })
+
+    //Text anzeigen, Nachricht gesendet
+    nameField.disabled = false;
+    emailField.disabled = false;
+    messageField.disabled = false;
+    sendButton.disabled = false;
+
+  }
 }
+
+
