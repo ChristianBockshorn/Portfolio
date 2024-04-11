9e986b4d10d1cb265d9cdd53d9ae84c8989b5027
Christian Bockshorn <bockshorn@gmx.net>
2024-04-11 10:48:12 +0200

    add join project

---
+++ src/app/landingPage/portfolio-section/projekt-join/projekt-join.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projekt-join',
+  standalone: true,
+  imports: [],
+  templateUrl: './projekt-join.component.html',
+  styleUrl: './projekt-join.component.scss'
+})
+export class ProjektJoinComponent {
+
+}
