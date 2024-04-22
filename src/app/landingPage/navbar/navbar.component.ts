a86242765cfaf45f5a47fb1e10f47addca392ccd
Christian Bockshorn <bockshorn@gmx.net>
2024-04-22 22:50:19 +0200

    edit portfolio section

--- src/app/landingPage/navbar/navbar.component.ts
+++ src/app/landingPage/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContactFormComponent } from "../contact-form/contact-form.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar',
+    standalone: true,
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss',
+    imports: [CommonModule, ContactFormComponent, FooterComponent]
 })
 export class NavbarComponent {
 
+isNavOpen: any;
+  openNav() {
+    this.isNavOpen = !this.isNavOpen; // Um den Zustand umzuschalten
+  
+  }
 }
