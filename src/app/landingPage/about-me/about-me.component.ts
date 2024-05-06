41a875833e58816bd335316343377c8d4f3c51f1
ChristianBockshorn <bockshorn@gmx.net>
2024-05-06 18:23:57 +0200

    edit about me

--- src/app/landingPage/about-me/about-me.component.ts
+++ src/app/landingPage/about-me/about-me.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
 
